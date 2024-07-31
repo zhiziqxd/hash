4dc404855611bf63d31e3978fa01221fe31083e5
zhiziqxd <166890319+zhiziqxd@users.noreply.github.com>
2024-07-31 22:17:56 +0800

    Update numhash.cpp

--- hash/numhash.cpp
+++ hash/numhash.cpp
@@ -2,21 +2,21 @@
 #include <string>
 using namespace std;
 string str;
-long long int h(string str){
+long long int h(string str,int n){
     long long int ans=0;
     long long int s[4]={0,1,1,1};//清零
     for(int i=0;i<str.length();i++){
         if(str[i]>='a'&&str[i]<='z'){
             ans+=str[i]*s[1];
-            s[1]*=26;
+            s[1]*=n;
         }
         else if(str[i]>='A'&&str[i]<='Z'){
             ans+=str[i]*s[2];
-            s[2]*=26;
+            s[2]*=n;
         }
         else if(str[i]>='0'&&str[i]<='9'){
             ans+=str[i]*s[3];
-            s[3]*=26;
+            s[3]*=n;
         }
         else{
             ans+=str[i];
@@ -26,6 +26,6 @@ long long int h(string str){
 }
 int main(){
     getline(cin,str);
-    cout<<h(str);
+    cout<<h(str，26);
     return 0;
 }
